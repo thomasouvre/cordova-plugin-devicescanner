cd93ccbaccca46b9c8b6c43799f7bed09ac0f4ab
ouvré thomas <t.ouvre@live.com>
2017-09-25 16:04:22 +0200

    Update DeviceScanner.java

--- src/android/DeviceScanner.java
+++ src/android/DeviceScanner.java
@@ -16,6 +16,7 @@ import org.apache.cordova.CordovaInterface;
 import org.apache.cordova.CordovaPlugin;
 import org.apache.cordova.CordovaWebView;
 import org.apache.cordova.LOG;
+import org.apache.cordova.PluginResult;
 
 import org.json.JSONArray;
 import org.json.JSONException;
